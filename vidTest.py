f30ed13841803ef319e6848456712b0fb55d36bb
Nicholas Simonelli <nrsimonelli@gmail.com>
2020-08-19 20:07:26 -0500

    full rest api

--- vidTest.py
+++ vidTest.py
@@ -2,8 +2,19 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/1", {"likes": 10, "name": "nick", "views": 23567})
-print(response.json())
+data = [{"likes": 10, "name": "nick", "views": 23567},
+        {"likes": 33, "name": "joe", "views": 96034},
+        {"likes": 25, "name": "devin", "views": 5567},
+        {"likes": 90, "name": "david", "views": 1135},
+        {"likes": 57, "name": "colleen", "views": 667567}]
+
+for i in range(len(data)):
+  response = requests.put(BASE + "video/" + str(i), data[i])
+  print(response.json())
+
 input()
-response = requests.get(BASE + "video/1", {"likes": 10})
+response = requests.delete(BASE + "video/0")
+print(response)
+input()
+response = requests.get(BASE + "video/2")
 print(response.json())
